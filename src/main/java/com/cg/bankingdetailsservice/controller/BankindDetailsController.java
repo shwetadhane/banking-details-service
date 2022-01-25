2c3f396eb46e3b1586d8cbe4ecc822ccd3f11f52
Admin <shwtjadhav@gmail.com>
2022-01-25 18:07:57 +0000

    added bankaccount class

--- src/main/java/com/cg/bankingdetailsservice/controller/BankindDetailsController.java
+++ src/main/java/com/cg/bankingdetailsservice/controller/BankindDetailsController.java
@@ -1,5 +1,6 @@
 package com.cg.bankingdetailsservice.controller;
 
+import com.cg.bankingdetailsservice.model.BankAccount;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class BankindDetailsController {
 
     @RequestMapping("/bankdetails")
-    public String getBankDetails(){
-        return "22021989";
+    public BankAccount getBankDetails(){
+        return new BankAccount("Shweta","123456789000");
     }
 }
